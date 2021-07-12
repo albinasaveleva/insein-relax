ea54a5a13af4711a64ce06a8817c97c324e85361
albinasaveleva <savelevalbina@gmail.com>
2021-07-12 17:08:19 +0300

    add handlerPopupConsultation

--- layout/src/index.js
+++ layout/src/index.js
@@ -2,11 +2,13 @@
 
 import bodyListeners from "./modules/bodyListeners";
 import handlerMenu from "./modules/handlerMenu";
+import handlerPopupConsultation from "./modules/handlerPopupConsultation";
 import handlerPopupPrivacy from "./modules/handlerPopupPrivacy";
 import handlerPopupRepairTypes from "./modules/handlerPopupRepairTypes";
 import handlerPopupTransparency from "./modules/handlerPopupTransparency";
 import scrollStart from "./modules/scrollStart";
 import transparencySlider from "./modules/transparencySlider";
+handlerPopupConsultation();
 transparencySlider();
 
 handlerPopupTransparency();
