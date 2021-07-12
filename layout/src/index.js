7a9ebde7a34af335250f2c07ba8265dc1a2cbccd
albinasaveleva <savelevalbina@gmail.com>
2021-07-12 19:12:30 +0300

    add handlerRepairTypes

--- layout/src/index.js
+++ layout/src/index.js
@@ -6,8 +6,10 @@ import handlerPopupConsultation from "./modules/handlerPopupConsultation";
 import handlerPopupPrivacy from "./modules/handlerPopupPrivacy";
 import handlerPopupRepairTypes from "./modules/handlerPopupRepairTypes";
 import handlerPopupTransparency from "./modules/handlerPopupTransparency";
+import handlerRepairTypes from "./modules/handlerRepairTypes";
 import scrollStart from "./modules/scrollStart";
 import transparencySlider from "./modules/transparencySlider";
+handlerRepairTypes();
 handlerPopupConsultation();
 transparencySlider();
 
@@ -18,4 +20,7 @@ bodyListeners();
 handlerMenu();
 scrollStart();
 
-window.addEventListener('resize', transparencySlider);
+window.addEventListener('resize', () => {
+    transparencySlider();
+    handlerRepairTypes();
+});
---
+++ layout/src/modules/handlerRepairTypes.js
@@ -0,0 +1,93 @@
+const handlerRepairTypes = () => {
+    const currentCount = document.querySelector('.slider-counter-content__current'),
+        totalCount = document.querySelector('.slider-counter-content__total');
+    let currentSlide = 0;
+    currentCount.textContent = 1;
+
+    const repairTypesTabs = () => {
+        const tabs = document.querySelectorAll('.repair-types-nav__item'), 
+            tabsContent = document.querySelectorAll('.types-repair');
+        
+        const toggleTabContent = (index) => {
+            tabsContent.forEach((item, i) => {
+                if (index === i) {
+                    tabs[i].classList.add('active');
+                    item.style.display = '';
+                } else {
+                    tabs[i].classList.remove('active');
+                    item.style.display = 'none';
+                }
+            });
+        };
+    
+        document.body.addEventListener('click', (event) => {
+            let target = event.target;
+            if (target.matches('.repair-types-nav__item')) {
+                currentCount.textContent = 1;
+                currentSlide = 0;
+
+                tabs.forEach((item, index) => {
+                    if (target === item) {
+                        toggleTabContent(index);
+                    }
+                });
+            }
+        });
+    };
+    const repairSlider = () => {
+        const arrowLeft = document.querySelector('#repair-types-arrow_left'),
+            arrowRight = document.querySelector('#repair-types-arrow_right'),
+            slides = document.querySelectorAll('.types-repair');
+
+        totalCount.textContent = slides.length;
+        currentCount.textContent = 1;
+        slides.forEach((slide, index) => {
+            if (index === currentSlide) {
+                return;
+            } else {
+                slide.style.display = 'none';
+            }
+        });
+    
+        const prevSlide = (elem, index, ) => {
+            elem[index].style.display = 'none';
+            currentCount.textContent = currentSlide + 1;
+        };
+        const nextSlide = (elem, index, ) => {
+            elem[index].style.display = '';
+            currentCount.textContent = currentSlide + 1;
+        };
+    
+        arrowLeft.addEventListener('click', () => {
+            prevSlide(slides, currentSlide);
+            currentSlide--;
+            if (currentSlide < 0) {
+                currentSlide = slides.length - 1;
+            }  
+            nextSlide(slides, currentSlide);      
+        });
+        arrowRight.addEventListener('click', () => {
+            prevSlide(slides, currentSlide);
+            currentSlide++;
+            if (currentSlide >= slides.length) {
+                currentSlide = 0;
+            }
+            nextSlide(slides, currentSlide);
+        });
+    };
+    const repairTypesSlider = () => {
+        const arrowLeft = document.querySelector('#nav-arrow-repair-left_base'),
+            arrowRight = document.querySelector('#nav-arrow-repair-right_base'),
+            slides = document.querySelectorAll('.repair-types-nav__item');
+        let transformX = 0,
+            maxLength = +getComputedStyle(slides[0]).width.slice(0, -2) * slides.length;
+        console.dir(maxLength);
+    };
+
+    repairTypesTabs();
+    repairSlider();
+    if (window.innerWidth <= 1024) {
+        repairTypesSlider();
+    }
+};
+export default handlerRepairTypes;
